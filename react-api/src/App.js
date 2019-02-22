3af66ff1df993d5a843041541487423e96143c06
dajmmos007 <dajmos007@gmail.com>
2019-02-22 11:17:44 +0100

    Adding foundation zubr framework

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './Sass/global.scss'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div className="index">
+        </div>
       </div>
     );
   }
